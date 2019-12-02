9c1949b4da5e5f213d628c99ccc14ac73a94b24c
Nero Nakamichi <nedew.sn@gmail.com>
2019-12-03 06:41:49 +0900

    BGMのエラーを修正

--- script.js
+++ script.js
@@ -162,19 +162,19 @@ const startGame = () => {
   // }
 
 
-  // let elmBGM = document.createElement('audio');
-  // elmBGM.setAttribute('src', 'sounds/bgm/bgm_maoudamashii_8bit22.mp3');
-  // elmBGM.setAttribute('loop', 'loop');
-  // elmBGM.setAttribute('autoplay', 'autoplay');
-  // elmBGM.id = 'bgm';
-  // elmBGM.volume = 0.1;
-  // document.getElementsByTagName('body')[0].appendChild(elmBGM);
+  let elmBGM = document.createElement('audio');
+  elmBGM.setAttribute('src', 'sounds/bgm/bgm_maoudamashii_8bit22.mp3');
+  elmBGM.setAttribute('loop', 'loop');
+  elmBGM.setAttribute('autoplay', 'autoplay');
+  elmBGM.id = 'bgm';
+  elmBGM.volume = 0.1;
+  document.getElementsByTagName('body')[0].appendChild(elmBGM);
 
-  // let elmSoundGameclear = document.createElement('audio');
-  // elmSoundGameclear.setAttribute('src', 'sounds/sound-effect/game_maoudamashii_9_jingle01.mp3');
-  // elmSoundGameclear.volume = 0.2;
-  // elmSoundGameclear.id = 'gameclear';
-  // document.getElementsByTagName('body')[0].appendChild(elmSoundGameclear);
+  let elmSoundGameclear = document.createElement('audio');
+  elmSoundGameclear.setAttribute('src', 'sounds/sound-effect/game_maoudamashii_9_jingle01.mp3');
+  elmSoundGameclear.volume = 0.2;
+  elmSoundGameclear.id = 'gameclear';
+  document.getElementsByTagName('body')[0].appendChild(elmSoundGameclear);
 
   let elmMainSE = document.createElement('audio');
   elmMainSE.setAttribute('src', 'sounds/sound-effect/se_maoudamashii_system37.mp3');
